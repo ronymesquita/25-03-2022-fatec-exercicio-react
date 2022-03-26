6c3ad56927f7caf3ec4f670cced7f0cfe4ad85fd
Rony Mesquita da Silva <ronymesquitadasilva@gmail.com>
2022-03-25 21:48:28 -0300

    Inclusão de arquivo HTML e JSX equivalente

--- src/index.js
+++ src/index.js
@@ -3,8 +3,42 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   return (
-    <div>
-      Meu primeiro componente React.
+    <div style={{
+      margin: 'auto',
+      width: 768,
+      backgroundColor: '#EEE',
+      padding: 12,
+      borderRadius: 8
+    }}>
+      <label htmlFor='nome' style={{
+        display: 'block',
+        marginBottom: '.25rem'
+      }}>
+        Nome
+      </label>
+      <input id='nome' type='text' style={{
+        paddingTop: '.5rem',
+        paddingBottom: '.5rem',
+        borderStyle: 'hidden',
+        outline: 'none',
+        borderRadius: '.5rem',
+        width: '100%'
+      }} />
+
+      <button style={{
+        marginTop: '.75rem',
+        paddingTop: '.5rem',
+        paddingBottom: '.5rem',
+        backgroundColor: 'blueviolet',
+        color: 'white',
+        border: 'none',
+        width: '100%',
+        borderRadius: '.5rem',
+        cursor: 'pointer',
+        boxSizing: 'border-box'
+      }}>
+        Enviar
+      </button>
     </div>
   )
 }
